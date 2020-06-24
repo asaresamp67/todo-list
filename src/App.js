04157f584c3d2cee041eab8c02d449e355ec7fb6
asaresamp67 <asaresamp67@gmail.com>
2020-06-24 20:49:22 +0200

    set up methods

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import TodoList from "./Components/TodoList";
 import TodoInput from "./Components/TodoInput";
 export default class App extends Component {
+  state = {
+    item: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "code" },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("handle Change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle Submit");
+  };
+  clearList = () => {
+    console.log("clear List");
+  };
+  handleDelete = (id) => {
+    console.log("handle delete ${id}");
+  };
+  handleEdit = (id) => {
+    console.log("handle edit ${id}");
+  };
   render() {
-    //hello git
+    console.log(this.state);
     return (
       <React.Fragment>
         <div className="container">
           <div className="row">
-            <div className="col">
-              <TodoInput />
-              <TodoList />
-              <TodoList />
+            <div className="col-10 col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">todo input</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+
+              <TodoList
+                items={this.state.items}
+                handleDelete={this.handleDelete}
+                clearList={this.clearList}
+                handleEdit={this.handleEdit}
+              />
             </div>
           </div>
         </div>
